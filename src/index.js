527c2ae9b421f45bd8fd40a76afd747e7933a9df
Italo Cedro <italo.sales@cadmus.com.br>
2022-06-21 10:19:31 -0300

    feat: adjustment in server on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'http://localhost';
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`🔥 Listening on ${HOST}:${PORT}`);
 });
